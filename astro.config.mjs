2ce140d54db042297ec6b56bbc2d7600d91afb3d
peter <peterkudelas@gmail.com>
2023-05-13 22:39:06 +0200

    added deno deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: deno()
 });
