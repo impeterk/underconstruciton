2f7cc15a60413c8dfd4a822385b73b547cb4a430
perer <peter@works7ation.com>
2023-08-07 00:26:55 +0200

    started over, w/o tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify/functions";
 import vercel from "@astrojs/vercel/serverless";
 
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: []
 });
